cff4476ef60b890474b730ab836f33752be0a6f1
hrist <hristov821@gmail.com>
2019-11-17 19:51:15 +0200

    Added conversation of kelvin to celsius.

--- script/show_map.js
+++ script/show_map.js
@@ -1,14 +1,5 @@
-/*
-    Here only map visualization can be left.
-    The other logic and map modification can be moved in requests.js. Make a function which accepts x and y.
-    It will be called from fetch_selected_city_coordinates when x and y are obtained.
-    It will fetch the weather for the given coordinates and visualize them properly on the map.
-    After all done don't forget to delete this comment, as by the clean code convention such comments are just noise.
-*/
-
 var x;
 var y;
-
 
 navigator.geolocation.getCurrentPosition(getPosition);
 
@@ -26,17 +17,9 @@ function getPosition(position){
         id: 'mapbox.streets'
     }).addTo(mymap);
 
-    L.marker([x, y]).addTo(mymap).bindPopup('You are currently here.').openPopup();
-
-
-    const request = new Request("https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat="+x+"&lon="+y+"&APPID=8d50b46972b36311b1bdd74f434be1b0");
-    
-    const gosho = fetch(request).then(response => {
-        if (response.status === 200) {
-            console.log(response.json());
-            return response.json();
-        }
-    });
+    var url = "http://127.0.0.1:5000/?latitude=" + x + "&longtitude=" + y;
+    var response = send_request(url, pasteDataOnMap);
+    pasteDataOnMap(response);
 }
 
 function move_to_postion(latitude,longtitude){
@@ -44,19 +27,18 @@ function move_to_postion(latitude,longtitude){
     y=longtitude
     mymap.setView([latitude, longtitude], 13);
 
-    //var url = "https://samples.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longtitude + "&appid=8d50b46972b36311b1bdd74f434be1b0"
-    //url ="https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longtitude+"&APPID=8d50b46972b36311b1bdd74f434be1b0"
     var url = "http://127.0.0.1:5000/?latitude=" + latitude + "&longtitude=" + longtitude
     var response = send_request(url, pasteDataOnMap);
 }
 
 function pasteDataOnMap(response){
-    console.log(response);
     var weather = response["main"];
 
     var temperature = weather["temp"];
     var pressure = weather["pressure"];
     var humidity = weather["humidity"];
+
+    var tempFromKelvinsToCelsius = weather["temp"] - 273.15;
     
-    L.marker([x, y]).addTo(mymap).bindPopup('Temperature : ' +temperature+", Presure : " + pressure + ", Humidity : "+ humidity).openPopup();
+    L.marker([x, y]).addTo(mymap).bindPopup("Temperature : " + tempFromKelvinsToCelsius + " °C, Pressure : " + pressure + " hPa, Humidity : "+ humidity + " %").openPopup();
 }
